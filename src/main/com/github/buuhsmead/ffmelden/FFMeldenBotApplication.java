66fc278df596e99c2c8be162f56f6a1f3a398b3f
Huub Daems <buuhsmead@gmail.com>
2018-01-01 22:33:12 +0100

    Basis router setup.

---
+++ src/main/com/github/buuhsmead/ffmelden/FFMeldenBotApplication.java
@@ -0,0 +1,17 @@
+package com.github.buuhsmead.ffmelden;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * This is a standard spring-boot main class.
+ */
+@SpringBootApplication
+public class FFMeldenBotApplication {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(FFMeldenBotApplication.class, args);
+    }
+
+}
